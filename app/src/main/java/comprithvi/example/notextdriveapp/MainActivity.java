2c2baa113e8fd055a94c1935346da528d2365b92
prith2014 <prith2014@gmail.com>
2018-03-05 23:31:21 -0500

    Prithvi branch - Saves selected bluetooth device's name and address to "bluetoothData.txt"

--- app/src/main/java/comprithvi/example/notextdriveapp/MainActivity.java
+++ app/src/main/java/comprithvi/example/notextdriveapp/MainActivity.java
@@ -3,9 +3,14 @@ package comprithvi.example.notextdriveapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+    String selectedDeviceName;
+    String selectedDeviceAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +32,18 @@ public class MainActivity extends AppCompatActivity {
     public void setupButton(View v) {
         Intent intent = new Intent(this, SetupActivity.class);
         startActivityForResult(intent, Intent_Constants.INTENT_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (resultCode == Intent_Constants.INTENT_RESULT_CODE) {
+
+            // Setup activity completed, get device name and address
+            Bundle extras = intent.getExtras();
+            assert extras != null;
+            selectedDeviceName = extras.getString("EXTRA_DEVICENAME");
+            selectedDeviceAddress = extras.getString("EXTRA_DEVICEADDRESS");
+
+        }
     }
 }
--- app/src/main/java/comprithvi/example/notextdriveapp/SetupActivity.java
+++ app/src/main/java/comprithvi/example/notextdriveapp/SetupActivity.java
@@ -2,12 +2,20 @@ package comprithvi.example.notextdriveapp;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
@@ -16,12 +24,75 @@ public class SetupActivity extends AppCompatActivity {
     public static final int REQUEST_ENABLE_BT = 2;
     ListView pairedDeviceListView;
     ArrayAdapter pairedDeviceAdapter;
-    ArrayList<HashMap<String,String>> pairedDevicesArrayList;
+    ArrayList<HashMap<String,String>> pairedDevicesList;
+    ArrayList<String> pairedDevicesListNames;
+    //ArrayAdapter<HashMap<String, String>> pairedDeviceAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        listPairedBlueToothDevices();
+
+        // Show list of paired devices
+        pairedDeviceListView = findViewById(R.id.pairedDevicesListView);
+        pairedDeviceAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pairedDevicesListNames);
+        pairedDeviceListView.setAdapter(pairedDeviceAdapter);
+        pairedDeviceAdapter.notifyDataSetChanged();
+
+        // Show list of paired devices ArrayList Hashmap verison (not pretty)
+        //pairedDeviceListView = findViewById(R.id.pairedDevicesListView);
+        //pairedDeviceAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pairedDevicesList);
+        //pairedDeviceListView.setAdapter(pairedDeviceAdapter);
+        //pairedDeviceAdapter.notifyDataSetChanged();
+
+        pairedDeviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                HashMap<String, String> selectedBTHashMap = pairedDevicesList.get(position);
+                //Set<String> bluetooth = selectedBTHashMap.keySet();
+                String selectedDeviceName = pairedDevicesListNames.get(position);
+                String selectedDeviceAddress = selectedBTHashMap.get(selectedDeviceName);
+
+                // Write Device name and address to text file
+                writeToFile(selectedDeviceName, getApplicationContext());
+                writeToFile(selectedDeviceAddress, getApplicationContext());
+
+                // Go back to main activity
+                Intent intent = new Intent(SetupActivity.this, MainActivity.class);
+                Bundle extras = new Bundle();
+                extras.putString("EXTRA_DEVICENAME", selectedDeviceName);
+                extras.putString("EXTRA_DEVICEADDRESS", selectedDeviceAddress);
+                //intent.putExtra("map", selectedBTHashMap);
+                intent.putExtras(extras);
+                //startActivity(intent);
+                setResult(Intent_Constants.INTENT_RESULT_CODE, intent);
+                finish();
+            }
+        });
+
+    }
+
+    // Function to write to file
+    public void writeToFile(String data, Context context) {
+
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("bluetoothData.txt", Context.MODE_APPEND));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Toast toast = Toast.makeText(this, "Saved to bluetoothData.txt", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    // Listing Bluetooth devices
+    public void listPairedBlueToothDevices() {
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter != null) {
@@ -34,10 +105,10 @@ public class SetupActivity extends AppCompatActivity {
             }
 
             Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-            ArrayList<String> pairedDevicesListNames = new ArrayList<String>();
-            ArrayList<String> pairedDevicesListAddresses = new ArrayList<String>();
+            pairedDevicesListNames = new ArrayList<>();
+            //ArrayList<String> pairedDevicesListAddresses = new ArrayList<String>();
             // make a hash map
-            HashMap<String, String> BTpairedDevices = new HashMap<>();
+            pairedDevicesList = new ArrayList<>();
 
             if (pairedDevices.size() > 0) {
                 // There are paired devices, get name and address of each paired device
@@ -46,23 +117,13 @@ public class SetupActivity extends AppCompatActivity {
                     String deviceHardwareAddress = device.getAddress(); // MAC address
 
                     pairedDevicesListNames.add(deviceName);
-                    pairedDevicesListAddresses.add(deviceHardwareAddress);
-                    BTpairedDevices.put(deviceName, deviceHardwareAddress);
+                    //pairedDevicesListAddresses.add(deviceHardwareAddress);
+                    HashMap<String, String> BTpairedDevicesHashes = new HashMap<>();
+                    BTpairedDevicesHashes.put(deviceName, deviceHardwareAddress);
+                    pairedDevicesList.add(BTpairedDevicesHashes);
                 }
             }
-
-            // Show list of paired devices
-            pairedDeviceListView = findViewById(R.id.pairedDevicesListView);
-            pairedDeviceAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pairedDevicesListNames);
-            pairedDeviceListView.setAdapter(pairedDeviceAdapter);
-            pairedDeviceAdapter.notifyDataSetChanged();
-
-
         }
-
     }
-
-    //
-
 
 }
