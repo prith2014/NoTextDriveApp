5b56f672d591ed3406cbfab204e4c7c7e017a407
Brett Lechner <blechner750@gmail.com>
2018-03-06 15:34:20 -0500

    Easy Merge

--- app/src/main/java/comprithvi/example/notextdriveapp/MainActivity.java
+++ app/src/main/java/comprithvi/example/notextdriveapp/MainActivity.java
@@ -23,88 +23,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private NotificationManager notificationManager;
-    private NotificationListenerService notificationListenerService;
-    private Notification builder;
-    String channelId = "default_channel_id";
-    String channelDescription = "Default Channel";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(notificationManager == null){
-            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        }
-        create_notification_channel();
-
-
-        Button send = findViewById(R.id.send);
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                postNotification("Title", "text");
-            }
-        });
-
-        Button start = findViewById(R.id.start);
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startService();
-            }
-        });
-
-        Button stop = findViewById(R.id.stop);
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopService();
-            }
-        });
-
-    }
-
-    void postNotification(String title, String text){
-        builder = new Notification.Builder(MainActivity.this, channelId)
-                .setContentTitle(title)
-                .setContentText(text)
-                .setSmallIcon(android.R.drawable.ic_popup_reminder)
-                .build();
-        notificationManager.notify(1, builder);
-    }
-
-    void create_notification_channel(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationChannel notificationChannel = notificationManager.getNotificationChannel(channelId);
-            if(notificationChannel == null){
-                int importance = NotificationManager.IMPORTANCE_HIGH;
-                notificationChannel = new NotificationChannel(channelId, channelDescription, importance);
-                notificationChannel.setLightColor(Color.GREEN);
-                notificationChannel.enableVibration(true);
-                notificationManager.createNotificationChannel(notificationChannel);
-            }
-        }
-    }
-
-    void startService(){
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if(!notificationManager.isNotificationPolicyAccessGranted()){
-            Toast.makeText(MainActivity.this, "Please activate Do Not Disturb Access and press Back to return to the application!", Toast.LENGTH_LONG).show();
-            startActivityForResult(new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS), 0);
-        }
-        else{
-            notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_PRIORITY);
-        }
-
-
-
-
-    }
-    void stopService(){
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
     }
 }
