1ea8c6a450fc902438d9441f86704e46f0952443
prith2014 <prith2014@gmail.com>
2018-04-17 16:06:16 -0400

    Attempt to merge Brett and Prithvi branches

--- app/src/main/java/comprithvi/example/notextdriveapp/BroadcastReceiverService.java
+++ app/src/main/java/comprithvi/example/notextdriveapp/BroadcastReceiverService.java
@@ -14,6 +14,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
@@ -22,13 +23,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.widget.Toast;
-
-import com.google.android.gms.location.ActivityRecognition;
-import com.google.android.gms.location.ActivityRecognitionResult;
-import com.google.android.gms.location.DetectedActivity;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -47,11 +41,13 @@ import static com.google.android.gms.location.LocationServices.getFusedLocationP
 public class BroadcastReceiverService extends Service {
     private static final String TAG = "myApp";
     private final int REQUEST_LOCATION = 200;
+    int softDisableTimer = 60000;           //milliseconds
     String blueToothAddress;
     IntentFilter filter;
     String ANDROID_CHANNEL_ID = "default_channel_id_2";
 
-    // Variables for Activity Recognition
+    Boolean isSoftDisableOn = false;
+    Boolean isBluetoothConnected = false;
 
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -60,8 +56,8 @@ public class BroadcastReceiverService extends Service {
 
             Log.v(TAG, "Broadcast Receiver onReceive function was called");
             String action = intent.getAction();
-            Log.v(TAG, action);
-            Log.v(TAG, BluetoothDevice.ACTION_FOUND);
+            //Log.v(TAG, action);
+            //Log.v(TAG, BluetoothDevice.ACTION_FOUND);
             if (blueToothAddress != null) {
                 Log.v(TAG, blueToothAddress);
             }
@@ -73,6 +69,7 @@ public class BroadcastReceiverService extends Service {
                 if (device.getAddress().equals(blueToothAddress)) {
                     // Car bluetooth is connected, time to measure speed/use accelerometer
                     Log.v(TAG, "YEAH CAR BLUETOOTH CONNECTED !!!!!!");
+                    isBluetoothConnected = true;
                     launchSpeedService();
                 }
             }
@@ -82,6 +79,7 @@ public class BroadcastReceiverService extends Service {
             if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
                 //Device has disconnected
                 Log.v(TAG, "YEAH BLUETOOTH DISCONNECTED !!!!!!");
+                isBluetoothConnected = false;
                 stopSpeedService();
             }
 
@@ -104,6 +102,12 @@ public class BroadcastReceiverService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.v(TAG, "Service in onStartCommand");
         //blueToothAddress = intent.getStringExtra("address");
+        Bundle args = intent.getExtras();
+
+        if (args != null) {
+            isSoftDisableOn = args.getBoolean("softDisable");
+            //Log.v("TAG", "Boolean value passed into BR service");
+        }
 
         Notification.Builder builder = new Notification.Builder(this, ANDROID_CHANNEL_ID)
                 .setContentTitle(getString(R.string.app_name))
@@ -114,6 +118,10 @@ public class BroadcastReceiverService extends Service {
         startForeground(1, notification);
 
         readFromFile(this);
+
+        if (isSoftDisableOn) {
+            softDisable();
+        }
 
         //return super.onStartCommand(intent, flags, startId);
         return START_STICKY;
@@ -148,11 +156,6 @@ public class BroadcastReceiverService extends Service {
         blueToothAddress = readStrings.get(1);
     }
 
-    public void launchBRService() {
-        Intent intent = new Intent(this, BroadcastReceiverService.class);
-        startService(intent);
-    }
-
     public void launchSpeedService() {
         Intent intent = new Intent(this, speedService.class);
 
@@ -167,14 +170,22 @@ public class BroadcastReceiverService extends Service {
         stopService(intent);
     }
 
-    public void setActivity(Intent intent) {
-        // Get update
-        ActivityRecognitionResult activityRecognitionResult = ActivityRecognitionResult.extractResult(intent);
+    public void softDisable() {
+        // Disables speed service for one minute
+        Log.v("TAG", "Soft Disable is stopping speed service");
+        stopSpeedService();
 
-        // Get most probable activity from list
-        //DetectedActivity mostProbableActivity = activityResult
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Log.v(TAG, "Soft Disable is over, restarting speed service");
+                isSoftDisableOn = false;
+                launchSpeedService();
+            }
+        };
+        Handler h = new Handler();
+        h.postDelayed(r, softDisableTimer);    // Timer till Do not Disturb turns on
     }
-
 
     @Nullable
     @Override
--- app/src/main/java/comprithvi/example/notextdriveapp/speedService.java
+++ app/src/main/java/comprithvi/example/notextdriveapp/speedService.java
@@ -5,8 +5,10 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -14,9 +16,12 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
+import android.provider.Telephony;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -30,6 +35,7 @@ public class speedService extends Service {
     private static final String TAG = "myApp";
     private final int REQUEST_LOCATION = 200;
     String ANDROID_CHANNEL_ID = "default_channel_id_2";
+    IntentFilter filter;
 
     // Speed variables
     long prevTime, currentTime, prevTime2;
@@ -46,10 +52,35 @@ public class speedService extends Service {
     Runnable r;
     Handler h = new Handler();
 
+    private final BroadcastReceiver sms = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.v("onReceive", "onReceive was called" );
+            if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+                Log.v("SMS Received", "SMS was received" );
+                String smsSender = "";
+                String smsBody = "";
+                for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                    smsSender = smsMessage.getDisplayOriginatingAddress();
+                    smsBody += smsMessage.getMessageBody();
+                    Toast.makeText(context, smsSender, Toast.LENGTH_LONG).show();
+
+                    // Sending text back
+                    sendSMS(smsSender, "Test Reply");
+                }
+            }
+        }
+    };
+
 
     public void onCreate() {
         Log.v(TAG, "Speed Service in onCreate");
         super.onCreate();
+
+        filter = new IntentFilter();
+        filter.setPriority(999);
+        filter.addAction(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+        registerReceiver(sms, filter);
     }
 
     @Override
@@ -71,7 +102,7 @@ public class speedService extends Service {
     }
 
 
-    //-------------------------------Location Functions-----------------------------------------
+    //--------------------------------------------Location Functions-----------------------------------------
 
     protected void startLocationUpdates() {
         // Create the location request to start receiving updates
@@ -146,7 +177,7 @@ public class speedService extends Service {
     }
 
 
-    //------------------------Notification Functions-------------------------
+    //----------------------------------------Notification Functions--------------------------------------
 
     // Check do not disturb permissions, and activate
     void startNotifBlock(){
@@ -167,7 +198,7 @@ public class speedService extends Service {
 
     public void checkSpeedAndBlock() {
         // Default speed that works is 2
-        if (speed > 5) {
+        if (speed > 0) {
             Log.v(TAG, "Notifications are being blocked");
             h.removeCallbacksAndMessages(null);     // Cancel timer
             counter = false;
@@ -186,14 +217,23 @@ public class speedService extends Service {
                     }
                 };
 
-                h.postDelayed(r, 20000);    // Timer till Do not Disturb turns on
+                h.postDelayed(r, timer);    // Timer till Do not Disturb turns on
             }
         }
     }
 
-    public void launchSpeedService() {
-        Intent intent = new Intent(this, speedService.class);
-        startService(intent);
+    //---------------------------------------------SMS FUNCTIONS---------------------------------------
+
+    public void sendSMS(String phoneNum, String msg) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNum, null, msg, null, null);
+            Log.v(TAG, "SMS Message sent");
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),ex.getMessage().toString(),
+                Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
     }
 
     @Nullable
@@ -207,6 +247,7 @@ public class speedService extends Service {
         Log.v(TAG, "Speed Service is destroyed");
         getFusedLocationProviderClient(this).removeLocationUpdates(mLocationCallBack);
         stopNotifBlock();
+        unregisterReceiver(sms);
         super.onDestroy();
     }
 }
