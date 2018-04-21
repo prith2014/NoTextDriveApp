589f979725ea9950ecd8019dd7a0ff799f476f41
Prithvi Venkat <prith2014@gmail.com>
2018-04-20 21:03:20 -0400

    Custom Listview in Bluetooth Setup

--- app/src/main/java/comprithvi/example/notextdriveapp/SetupActivity.java
+++ app/src/main/java/comprithvi/example/notextdriveapp/SetupActivity.java
@@ -10,10 +10,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -42,6 +45,8 @@ public class SetupActivity extends AppCompatActivity {
 
         listPairedBlueToothDevices();
 
+        // ARRAY ADAPTER
+        /*
         // Show list of paired devices
         pairedDeviceListView = findViewById(R.id.pairedDevicesListView);
         pairedDeviceAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pairedDevicesListNames);
@@ -80,7 +85,35 @@ public class SetupActivity extends AppCompatActivity {
                 finish();
             }
         });
+        */
 
+        // CUSTOM ADAPTER
+        pairedDeviceListView = findViewById(R.id.pairedDevicesListView);
+        CustomAdapterBluetooth customAdapterBluetooth = new CustomAdapterBluetooth();
+        pairedDeviceListView.setAdapter(customAdapterBluetooth);
+
+        pairedDeviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                HashMap<String, String> selectedBTHashMap = pairedDevicesList.get(position);
+                String selectedDeviceName = pairedDevicesListNames.get(position);
+                String selectedDeviceAddress = selectedBTHashMap.get(selectedDeviceName);
+                String bluetoothToTxt = (selectedDeviceName + '\n' + selectedDeviceAddress);
+
+                // Write Device name and address to text file
+                writeToFile(bluetoothToTxt, getApplicationContext());
+                markBluetooth();
+
+                // Go back to main activity
+                Intent intent = new Intent(SetupActivity.this, MainActivity.class);
+                Bundle extras = new Bundle();
+                extras.putString("EXTRA_DEVICENAME", selectedDeviceName);
+                extras.putString("EXTRA_DEVICEADDRESS", selectedDeviceAddress);
+                intent.putExtras(extras);
+                setResult(Intent_Constants.INTENT_RESULT_CODE, intent);
+                finish();
+            }
+        });
     }
 
     // Function to write to file
@@ -142,4 +175,30 @@ public class SetupActivity extends AppCompatActivity {
         }
     }
 
+    class CustomAdapterBluetooth extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return pairedDevicesList.size();
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.custom_listview_bluetooth,null);
+            TextView textView_bluetooth = view.findViewById(R.id.textView_bluetooth);
+
+            textView_bluetooth.setText(pairedDevicesListNames.get(i));
+            return view;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+    }
 }
+
